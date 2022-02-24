dbf2913e15fbf04283d2096aa8b8b2a8bdf8bbdc
Kevinadriann <97919504+Kevinadriann@users.noreply.github.com>
2022-02-24 20:01:16 +0700

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -75,7 +75,7 @@ async def typewriter(typew):
 @register(outgoing=True, pattern='^V(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("**MACAM BAGUS AE LU BEGITU KONTOL!!**")
+    await typew.edit("**PUJA TUHAN @sylucaz **")
 
 
 @register(outgoing=True, pattern='^J(?: |$)(.*)')
